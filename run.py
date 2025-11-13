daeabc029027927d753f8e27c5f7133bc4af00f5
Shivangid2904 <162672523+Shivangid2904@users.noreply.github.com>
2025-11-13 20:56:03 +0530

    final

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from crawler.main import start_crawler
 
 if __name__ == "__main__":
-    start_crawler("http://github.com", max_threads=5, max_depth=2)
+    start_crawler("http://gutenberg.org", max_threads=5, max_depth=2)
